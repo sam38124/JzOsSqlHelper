6655829d478c02be8de3d381aebf3ac444b7f478
sam38124 <sam38124@gmail.com>
2020-02-03 05:04:34 -0800

    0203

--- JzOsSqlHelperDemo/ViewController.swift
+++ JzOsSqlHelperDemo/ViewController.swift
@@ -17,16 +17,16 @@ class ViewController: UIViewController,Sql_Result{
         super.viewDidLoad()
         let a=SqlHelper("12.db")
         
-//        if(a.autoCreat()){
-//            a.exSql("CREATE TABLE  IF NOT EXISTS `logtable` ( id INTEGER PRIMARY KEY AUTOINCREMENT, data VARCHAR NOT NULL, type VARCHAR NOT NULL);")
-//            a.exSql("insert into logtable(data,type) values ('hello','sql')")
-//            a.query("select * from `logtable`",self)
-//        }
+        if(a.autoCreat()){
+            a.exSql("CREATE TABLE  IF NOT EXISTS `logtable` ( id INTEGER PRIMARY KEY AUTOINCREMENT, data VARCHAR NOT NULL, type VARCHAR NOT NULL);")
+            a.exSql("insert into logtable(data,type) values ('hello','sql')")
+            a.query("select * from `logtable`",self)
+        }
         
-//        if(a.initByBundleMain("MMY_EU_list_V0.4_190926","db")){
-//            a.query("select * from `Summary table`",self)
-//         a.query("select count(1) from `Summary table`",self)
-//        }
+        if(a.initByBundleMain("MMY_EU_list_V0.4_190926","db")){
+            a.query("select * from `Summary table`",self)
+         a.query("select count(1) from `Summary table`",self)
+        }
         
         DispatchQueue.global().async {
             if(a.initByUrl("https://bento2.orange-electronic.com/Orange%20Cloud/Database/MMY/EU/MMY_EU_list_V0.6_191128.db")){
