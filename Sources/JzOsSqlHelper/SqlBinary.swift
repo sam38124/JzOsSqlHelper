4979910f22ad66ac8e7499df86822bcffdc22238
sam38124 <sam38124@gmail.com>
2021-04-20 14:55:44 +0800

    0304

--- Sources/JzOsSqlHelper/SqlBinary.swift
+++ Sources/JzOsSqlHelper/SqlBinary.swift
@@ -13,8 +13,13 @@ public class SqlBinary {
     init(_ statement:OpaquePointer){
         self.statement=statement
     }
-    public func getString(_ index:Int)->String{
-        return  String(cString: sqlite3_column_text(statement,Int32(index))!)
+    public func getString(_ index:Int)->String?{
+        let text=sqlite3_column_text(statement,Int32(index))
+        if(text == nil){
+            return nil
+        }else{
+            return  String(cString: text!)
+        }
     }
     public func getInt(_ index:Int)->Int{
         return  Int(sqlite3_column_int(statement,Int32(index)))
@@ -31,6 +36,6 @@ public class SqlBinary {
         return Int(sqlite3_column_count(statement));
     }
     public func getColumnsName(_ index:Int)->String{
-      return    String(cString: sqlite3_column_name(statement, Int32(index)))
+        return String(cString: sqlite3_column_name(statement, Int32(index)))
     }
 }
